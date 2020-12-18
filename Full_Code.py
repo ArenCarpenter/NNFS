7e5a59067ff37f695922c2fb75f2db2486b06a1e
Aren Carpenter <14arenc@gmail.com>
2020-12-18 14:22:40 -0700

    Add Adagrad optimizer class

--- Full_Code.py
+++ Full_Code.py
@@ -150,6 +150,33 @@ class Optimizer_SGD:
         self.iterations += 1
 
 
+class Optimizer_Adagrad:
+    def __init__(self, learning_rate=1., decay=0., epsilon=1e-7):
+        self.learning_rate = learning_rate
+        self.current_learning_rate = learning_rate
+        self.decay = decay
+        self.iterations = 0
+        self.epsilon = epsilon
+
+    def pre_update_params(self):
+        if self.decay:
+            self.current_learning_rate = self.learning_rate * (1. / (1. + self.decay * self.iterations))
+
+    def update_params(self, layer):
+        if not hasattr(layer, 'weight_cache'):
+            layer.weight_cache = np.zeros_like(layer.weights)
+            layer.bias_cache = np.zeros_like(layer.biases)
+
+        layer.weight_cache += layer.dweights ** 2
+        layer.bias_cache += layer.dbiases ** 2
+
+        layer.weights += -self.current_learning_rate * layer.dweights / (np.sqrt(layer.weight_cache) + self.epsilon)
+        layer.biases += -self.current_learning_rate * layer.dbiases / (np.sqrt(layer.bias_cache) + self.epsilon)
+
+    def post_update_params(self):
+        self.iterations += 1
+
+
 X, y = spiral_data(100, 3)
 
 dense1 = Layer_Dense(2, 64)
@@ -160,7 +187,7 @@ dense2 = Layer_Dense(64, 3)
 
 loss_activation = Activation_Softmax_Loss_CategoricalCrossentropy()
 
-optimizer = Optimizer_SGD(decay=0.001, momentum=0.9)
+optimizer = Optimizer_Adagrad(decay=1e-4)
 
 for epoch in range(10001):
 
