84f1a0ae531d83766cc75cebac21a736282f5412
Aren Carpenter <14arenc@gmail.com>
2020-12-20 17:34:23 -0700

    Add testing set for validation

--- Full_Code.py
+++ Full_Code.py
@@ -289,3 +289,20 @@ for epoch in range(10001):
     optimizer.update_params(dense1)
     optimizer.update_params(dense2)
     optimizer.post_update_params()
+
+# Validate the model with a test set
+X_test, y_test = spiral_data(100, 3)
+
+dense1.forward(X_test)
+
+activation1.forward(dense1.output)
+
+dense2.forward(activation1.output)
+
+loss = loss_activation.forward(dense2.output, y_test)
+
+predictions = np.argmax(loss_activation.output, axis=1)
+if len(y_test.shape) == 2:
+    y_test = np.argmax(y_test, axis=1)
+accuracy = np.mean(predictions == y_test)
+print(f'validation, acc: {accuracy:.3f}, loss: {loss:.3f}')
